8b0add99ce33407538997736046355c58112ec16
Murilo Augusto <38955899+muriloaugusto11@users.noreply.github.com>
2019-04-01 10:19:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,36 @@ import getpass
 
 print("--||Cadastro||--")
 
-#nome = input("Digite seu nome: ")
-#endereço = input("Digite seu endereço: ")
-#sexo = input("Digite seu sexo (Feminino/Masculino): " ).upper()
-#while sexo != "F" and sexo != "M" and sexo != "FEMININO" and sexo != "MASCULINO":
-#  print("Sexo Inválido!")
-#  sexo = input("Digite seu sexo (Feminino(F)/Masculino(M): " )
-#print(sexo)
+nome = input("Digite seu nome: ")
+endereço = input("Digite seu endereço: ")
+sexo = input("Digite seu sexo (Feminino/Masculino): " ).upper()
+while sexo != "F" and sexo != "M" and sexo != "FEMININO" and sexo != "MASCULINO":
+  print("Sexo Inválido!")
+  sexo = input("Digite seu sexo (Feminino(F)/Masculino(M): " )
+print(sexo)
 
 #(falta limitar a apenas números)
-#cpf = input("Digite seu cpf: ")
-#while len(cpf) != 11:
-#  if len(cpf) < 11:
-#    print("Tamanho de CPF menor que o permitido!")
-#    print("O campo CPF deve ter 11 caracteres e você forneceu", len(cpf))
-#    cpf = input("Digite seu cpf: ")
-#  if len(cpf) > 11:
-#    print("Tamanho de CPF maior que o permitido!")
-#    print("O campo CPF deve ter 11 caracteres e você forneceu", len(cpf))
-#    cpf = input("Digite novamente seu CPF: ")  
+cpf = input("Digite seu cpf: ")
+while len(cpf) != 11:
+  if len(cpf) < 11:
+    print("Tamanho de CPF menor que o permitido!")
+    print("O campo CPF deve ter 11 caracteres e você forneceu", len(cpf))
+    cpf = input("Digite seu cpf: ")
+  if len(cpf) > 11:
+    print("Tamanho de CPF maior que o permitido!")
+    print("O campo CPF deve ter 11 caracteres e você forneceu", len(cpf))
+    cpf = input("Digite novamente seu CPF: ")  
 
 #(falta limitar a apenas números)
-#celular = input("Digite seu celular: ")
-#while len(celular) != 11:
-#  print("Tamanho inválido, você forneceu", len(celular),
-#   "quando na verdade deve ser 11, ")
-#  celular = input("Digite o número de celular novamente: ")
+celular = input("Digite seu celular: ")
+while len(celular) != 11:
+   print("Tamanho inválido, você forneceu", len(celular), "quando na verdade deve ser 11, ")
+   celular = input("Digite o número de celular novamente: ")
 
-senha = input("Senha: ")
+senha = input("Digite uma Senha: ")
+while len(senha) < 8 or len(senha) > 16:
+  print("Senha inválida, a senha deve ter no mínimo 8 caractéres e no máximo 16 caractéres e você forneceu", len(senha))
+  senha = input("Digite uma Senha novamente: ")
 conf_senha = input("Confirmação de senha: ")
 while senha != conf_senha:
   print("As senhas não batem! Digite novamente")
