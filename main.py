09312462e4576bd1ee97c7bea61be5ed2b03eca3
Murilo Augusto <38955899+muriloaugusto11@users.noreply.github.com>
2019-04-01 09:56:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+import getpass
+
 print("--||Cadastro||--")
 
-senha = 0
-senha = input("Digite sua senha de acesso: ")
+#nome = input("Digite seu nome: ")
+#endereço = input("Digite seu endereço: ")
+#sexo = input("Digite seu sexo (Feminino/Masculino): " ).upper()
+#while sexo != "F" and sexo != "M" and sexo != "FEMININO" and sexo != "MASCULINO":
+#  print("Sexo Inválido!")
+#  sexo = input("Digite seu sexo (Feminino(F)/Masculino(M): " )
+#print(sexo)
+cpf = input("Digite seu cpf: ")
+while len(cpf) != 11:
+  if len(cpf) < 11:
+    print("Tamanho de CPF menor que o permitido!")
+    print("O campo CPF deve ter 11 caracteres e você forneceu", len(cpf))
+    cpf = input("Digite seu cpf: ")
+  if len(cpf) > 11:
+    print("Tamanho de CPF maior que o permitido!")
+    print("O campo CPF deve ter 11 caracteres e você forneceu", len(cpf))
+    cpf = input("Digite novamente seu CPF: ")  
+
+celular = input("Digite seu celular: ")
+senha = input("")
+conf_senha = input("")
+
+senha = getpass.getpass("Digite sua senha de acesso: ")
 while senha != '123321':
   if senha == '123321':
-    print("ok")
+    print()
   else:
-    senha = input("Digite uma senha válida: ")
-  
+    senha = getpass.getpass("Digite uma senha válida: ")
+print("Seja bem vindo!")
+
