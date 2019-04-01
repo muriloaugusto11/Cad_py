7537be74cb45fbfacb83cf2abe6aa1f6f9d38c99
Murilo Augusto <38955899+muriloaugusto11@users.noreply.github.com>
2019-04-01 14:08:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,77 +2,86 @@ import getpass
 
 def cad():
 
-    print("--||Cadastro||--")
+    print("_\|/__Cadastro__\|/_")
+    print("_/|\__Alternat__/|\_")
 
-    name = input("Digite um nome: ")
-    while len(name) <3:
-      print("Nome inválido, o tamanho mínimo de um nome deve ser 3 e você digitou:", len(name))
-      name = input("Digite um nome válido: ")
+    #name = input("TYPE A NAME: ")
+    #while len(name) <3:
+    #  print("Nome inválido, o tamanho mínimo de um nome deve ser 3 e você digitou:", len(name))
+    #  name = input("TYPE A VALID NAME: ")
 
-    cep = input("Digite um CEP: ")
-    print(cep)
-    address = input("Digite um endereço: ")
-    print(address)
+    #cep = input("TYPE A CEP: ")
+    #print(cep)
+    #address = input("TYPE AN ADDRESS: ")
+    #print(address)
 
-    sex = input("Digite um sexo (Feminino/Masculino): ").upper()
-    while sex != "F" and sex != "M" and sex != "FEMININO" and sex != "MASCULINO":
-      print("Sexo Inválido!")
-      sex = input("Digite seu Sexo (Feminino(F)/Masculino(M): ")
-    print(sex)
+    #sex = input("ENTER A SEX (FEMALE(F)/MALE(M)): " ).upper()
+    #while sex != "F" and sex != "M" and sex != "FEMININO" and #sex != "MASCULINO":
+    #  print("INVALID SEX!")
+    #  sex = input("ENTER THE SEX (FEMALE(F)/MALE(M): ")
+    #  print(sex)
 
-    #(falta limitar a apenas números)
-    cpf = input("Digite um CPF válido: ")
-    while len(cpf) != 11:
-      if len(cpf) < 11:
-        print("Tamanho de CPF menor que o permitido!")
-        print("O campo CPF deve ter 11 caracteres e você forneceu: ", len(cpf))
-        cpf = input("Digite um CPF válido: ")
-      if len(cpf) > 11:
-        print("Tamanho de CPF maior que o permitido!")
-        print("O campo CPF deve ter 11 caracteres e você forneceu: ", len(cpf))
-        cpf = input("Digite um CPF válido: ")  
+    ##(falta limitar a apenas números)
+    #cpf = input("ENTER A VALID CPF: ")
+    #while len(cpf) != 11:
+    #  if len(cpf) < 11:
+    #    print("Tamanho de CPF menor que o permitido!")
+    #    print("O campo CPF deve ter 11 caracteres e você forneceu: ", len(cpf))
+    #    cpf = input("ENTER A VALID CPF: ")
+    #  if len(cpf) > 11:
+    #    print("Tamanho de CPF maior que o permitido!")
+    #    print("O campo CPF deve ter 11 caracteres e você forneceu: ", len(cpf))
+    #    cpf = input("ENTER A VALID CPF: ")  
 
-    #(falta limitar a apenas números)
-    cel = input("Digite um celular válido: ")
-    while len(cel) != 11:
-      print("Tamanho inválido, você forneceu", len(cel), "quando na verdade deve ser 11")
-      cel = input("Digite o Celular válido: ")
+    ##(falta limitar a apenas números)
+    #cel = input("ENTER A VALID MOBILE NUMBER: ")
+    #while len(cel) != 11:
+    #  print("Tamanho inválido, você forneceu", len(cel), "quando na verdade deve ser 11")
+    #  cel = input("ENTER A VALID MOBILE NUMBER: ")
 
-    #(falta limitar caracteres especiais)
-    login = input("Digite um Login válido: ")
+    ##(falta limitar caracteres especiais)
+    login = input("ENTER A VALID LOGIN: ")
     while len(login) <3 or len(login) > 16:
       print("Login inválido, o Login deve ter no 3 caractéres e no máximo 16 caractéres e você forneceu: ", len(login))
-      login = input("Digite um Login válido: ")
+      login = input("ENTER A VALID LOGIN: ")
 
     #(Falta obrigar uma letra maiuscula e uma mínima e aumentar o tamanho mínimo para 10)
-    password = input("Digite uma Senha válida: ")
+    password = input("ENTER A VALID PASSWORD: ")
     while len(password) < 8 or len(password) > 16:
       print("Senha inválida, a Senha deve ter no mínimo 8 caractéres e no máximo 16 caractéres e você forneceu: ", len(password))
-      password = input("Digite uma Senha válida: ")
+      password = input("ENTER A VALID PASSWORD: ")
 
-    c_password = input("Confirmação de Senha: ")
+    c_password = input("ENTER A VALID PASSWORD AGAIN TO CONFIRM: ")
     while password != c_password:
-      print("As senhas não batem! Digite uma Senha novamente: ")
-      password = input("Senha: ")
-      c_password = input("Confirmação de Senha: ")
-        
-    l_login = input("Digite a Login: ")    
-    p_password = getpass.getpass("Digite a Senha: ")    
-    while l_login != login and p_password != password:
-      print("Login ou Senha inválidos!")
-      l_login = input("Digite um Login válido: ")
-      p_password = getpass.getpass("Digite uma Senha válida: ")
-      if l_login == login and p_password == password:
-        print("ok")
-      else:
-        print()
+      print("PASSWORDS DO NOT MATCH! ENTER A PASSWORD AGAIN: ")
+      password = input("PASSWORD: ")
+      c_password = input("PASSWORD CONFIRMATION: ")
+
+def log():
+
+    print(login)
+    print(password)
+    #l_login = input("ENTER A LOGIN: ")    
+    #p_password = getpass.getpass("ENTER A PASSWORD: ")    
+    #while l_login != login and p_password != password:
+    #  print("INVALID LOGIN OR PASSWORD!")
+    #  l_login = input("ENTER A VALID LOGIN: ")
+    #  p_password = getpass.getpass("ENTER A VALID PASSWORD: ")
+    #  if l_login == login and p_password == password:
+    #    print("Corretos")
+    #  else:
+    #    print()
 
 cad()
 
-a = input("Deseja cadastrar mais um usuário? S/N: ").upper()
+a = "0"
 while a != "S" and a != "N":
-  a = input("Deseja cadastrar mais um usuário? S/N: ").upper()
-  if cad == "S":
+  a = input("Deseja cadastrar mais um usuário? S/N ou Login L: ").upper()
+  if a == "S":
     cad()
-  else:
-    print()
+  elif a == "N":
+    print("xs")
+  elif a == "L":
+    log()
+
+  
