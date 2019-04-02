24ec70064373020f26d8a5d737f898305d1d4c7f
Murilo Augusto <38955899+muriloaugusto11@users.noreply.github.com>
2019-04-02 12:48:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,39 +20,50 @@ def menu():
 
 def cad():
     print("\n Cadastro:")
-    name()
+    cad_name()
 
-def name():
+def cad_name():
     validation_name = input("TYPE A NAME: ")
-
-    while len(validation_name) < 6:
+    if len(validation_name) < 6:
       print("Nome inválido, o tamanho mínimo de um nome deve ser 6 e você digitou:", len(validation_name))
-      validation_name = input("TYPE A VALID NAME: ")
+      cad_name()
 
-    if re.match('^[a-zA-Z0-9_]+$',validation_name): 
-      name = validation_name
-      cep()
+    elif len(validation_name) > 6:
+      if re.match('^[a-zA-Z ]+$', validation_name): 
+        name = validation_name
+        print(name)
+        cad_cep()
+      else:
+        print("Nome inválido!")
+      cad_name()
+
+def cad_cep():
+    validation_cep = input("TYPE A CEP: ")
+    if re.match('^[0-9]+$', validation_cep):
+      cep = validation_cep
     else:
-      print("Nome inválido!")
-      name()
+      print("CEP inválido!")
+      cad_cep()
+    cad_address()
 
-def cep():
-    cep = input("TYPE A CEP: ")
-    address()
+def cad_address():
+    validation_address = input("TYPE AN ADDRESS: ")
+    if re.match('^[a-zA-Z ]+$', validation_address):
+      address = validation_address
+    else:
+      print("Invalid Address")
+      cad_address
+    cad_sex()
 
-def address():
-    address = input("TYPE AN ADDRESS: ")
-    sex()
-
-def sex():
+def cad_sex():
     sex = input("ENTER A SEX (FEMALE(F)/MALE(M)): " ).upper()
     while sex != "F" and sex != "M" and sex != "FEMININO" and sex != "MASCULINO":
       print("INVALID SEX!")
       sex = input("ENTER THE SEX (FEMALE(F)/MALE(M): ")
       print(sex)
-    cpf()
+    cad_cpf()
 
-def cpf():
+def cad_cpf():
     ##(falta limitar a apenas números)
     cpf = int(input("ENTER A VALID CPF: "))
 
@@ -66,25 +77,25 @@ def cpf():
         print("Tamanho de CPF maior que o permitido!")
         print("O campo CPF deve ter 11 caracteres e você forneceu: ", len(cpf))
         cpf = input("ENTER A VALID CPF: ")  
-      cel()
+      cad_cel()
 
-def cel():
+def cad_cel():
     ##(falta limitar a apenas números)
     cel = input("ENTER A VALID MOBILE NUMBER: ")
     while len(cel) != 11:
       print("Tamanho inválido, você forneceu", len(cel), "quando na verdade deve ser 11")
       cel = input("ENTER A VALID MOBILE NUMBER: ")
-    login()
+    cad_login()
 
-def login():
+def cad_login():
     ##(falta limitar caracteres especiais)
     login = input("ENTER A VALID LOGIN: ")
     while len(login) <3 or len(login) > 16:
       print("Login inválido, o Login deve ter no 3 caractéres e no máximo 16 caractéres e você forneceu: ", len(login))
       login = input("ENTER A VALID LOGIN: ")
-    password()
+    cad_password()
 
-def password():
+def cad_password():
     ##(Falta obrigar uma letra maiuscula e uma mínima e aumentar o tamanho mínimo para 10)
     password = input("ENTER A VALID PASSWORD: ")
     while len(password) < 8 or len(password) > 16:
